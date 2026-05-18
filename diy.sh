5fe126d13dd45c080daaf4a9191cdc4c953eafbe
Kuailedeqingnian <1783286818yun@gmail.com>
2026-05-18 18:37:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+set -e
 
 echo "DIY script start..."
 
-# 修改默认IP
+# 默认 IP 改为 192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 设置默认主题 Argon
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+# 默认主机名
+sed -i 's/OpenWrt/TR3000-OpenWrt/g' package/base-files/files/bin/config_generate
+
+# 默认主题改为 Argon
+if [ -f feeds/luci/collections/luci/Makefile ]; then
+  sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+fi
 
 # root 默认空密码
-sed -i 's/root::0:0:99999:7:::/root:::0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's#^root:[^:]*:#root::#' package/base-files/files/etc/shadow
 
-# 删除 picoclaw（如果存在）
-rm -rf package/*/picoclaw*
-rm -rf feeds/*/picoclaw*
+# 删除 picoclaw 相关内容
+find package feeds -iname '*picoclaw*' -exec rm -rf {} + || true
 
 echo "DIY script done."
