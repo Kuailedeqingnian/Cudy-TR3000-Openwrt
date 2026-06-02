f5a38629098ae85ea08375a8d8616eae668d8b5f
Kuailedeqingnian <1783286818yun@gmail.com>
2026-06-02 20:24:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/cu
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/custom/luci-app-argon-config
 
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/TR3000-OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Cudy-TR3000-OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's#^root:[^:]*:#root::#' package/base-files/files/etc/shadow
 
 find package feeds -iname '*picoclaw*' -exec rm -rf {} + || true
@@ -20,6 +20,7 @@ mkdir -p files/etc/uci-defaults
 cat > files/etc/uci-defaults/99-default-argon <<'EOF'
 #!/bin/sh
 uci set luci.main.mediaurlbase='/luci-static/argon'
+uci set luci.main.lang='zh_cn'
 uci commit luci
 exit 0
 EOF
